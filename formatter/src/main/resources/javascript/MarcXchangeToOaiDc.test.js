4fc9c450febc952690b1f3d512688ea54e3724f6
Linda Schwartz Karlsen <lsk@dbc.dk>
2017-02-06 17:15:35 +0000

    rawrepo-oai-formatter Beginning module and unit tests to create dc format (not yet tested) Search US#2010
    git-svn-id: https://svn.dbc.dk/repos/rawrepo-oai/trunk@109769 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ formatter/src/main/resources/javascript/MarcXchangeToOaiDc.test.js
@@ -0,0 +1,123 @@
+/** @file Module that contains unit tests for functions in MarcXchangeToOaiDc module */
+
+use( "MarcXchangeToOaiDc" );
+use( "UnitTest" );
+
+UnitTest.addFixture( "test createDcXml", function( ) {
+
+    var recordString = (
+        '<marcx:collection xmlns:marcx="info:lc/xmlns/marcxchange-v1">' +
+            '<marcx:record format="danMARC2" type="Bibliographic">' +
+            '<marcx:leader>00000n    2200000   4500</marcx:leader>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="001">' +
+                    '<marcx:subfield code="a">23645564</marcx:subfield>' +
+                    '<marcx:subfield code="b">870970</marcx:subfield>' +
+                    '<marcx:subfield code="c">20160525133413</marcx:subfield>' +
+                    '<marcx:subfield code="d">20010824</marcx:subfield>' +
+                    '<marcx:subfield code="f">a</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="004">' +
+                    '<marcx:subfield code="r">n</marcx:subfield>' +
+                    '<marcx:subfield code="a">e</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="008">' +
+                    '<marcx:subfield code="t">m</marcx:subfield>' +
+                    '<marcx:subfield code="u">r</marcx:subfield>' +
+                    '<marcx:subfield code="a">2001</marcx:subfield>' +
+                    '<marcx:subfield code="z">2003</marcx:subfield>' +
+                    '<marcx:subfield code="b">dk</marcx:subfield>' +
+                    '<marcx:subfield code="d">x</marcx:subfield>' +
+                    '<marcx:subfield code="j">f</marcx:subfield>' +
+                    '<marcx:subfield code="l">dan</marcx:subfield>' +
+                    '<marcx:subfield code="v">0</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="009">' +
+                    '<marcx:subfield code="a">a</marcx:subfield>' +
+                '<marcx:subfield code="g">xx</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="021">' +
+                    '<marcx:subfield code="a">87-7724-857-0</marcx:subfield>' +
+                    '<marcx:subfield code="c">ib.</marcx:subfield>' +
+                    '<marcx:subfield code="d">kr. 169,95</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="041">' +
+                    '<marcx:subfield code="a">dan</marcx:subfield>' +
+                    '<marcx:subfield code="c">jpn</marcx:subfield>' +
+                '</marcx:datafield>' +
+                '<marcx:datafield ind1="0" ind2="0" tag="100">' +
+                    '<marcx:subfield code="a">Murakami</marcx:subfield>' +
+                '<marcx:subfield code="h">Haruki</marcx:subfield>' +
+            '</marcx:datafield>' +
+            '<marcx:datafield ind1="0" ind2="0" tag="241">' +
+                '<marcx:subfield code="a">Nejimaki-dori kuronikure</marcx:subfield>' +
+            '</marcx:datafield>' +
+            '<marcx:datafield ind1="0" ind2="0" tag="245">' +
+                '<marcx:subfield code="a">Traekopfuglens kroenike</marcx:subfield>' +
+                '<marcx:subfield code="e">Haruki Murakami</marcx:subfield>' +
+                '<marcx:subfield code="f">oversat af Mette Holm</marcx:subfield>' +
+                '</marcx:datafield>' +
+            '<marcx:datafield ind1="0" ind2="0" tag="260">' +
+                '<marcx:subfield code="a">Aarhus</marcx:subfield>' +
+                '<marcx:subfield code="b">Klim</marcx:subfield>' +
+                '<marcx:subfield code="c">2003</marcx:subfield>' +
+                '<marcx:subfield code="k">tr.i udl.</marcx:subfield>' +
+                '</marcx:datafield>' +
+            '</marcx:record>' +
+        '</marcx:collection>'
+    );
+
+    var expected = XmlUtil.fromString(
+        '<oai_dc:dc ' +
+        'xmlns:dc="http://purl.org/dc/elements/1.1/" ' +
+        'xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" ' +
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">' +
+            '<dc:title>Trækopfuglens krønike</dc:title>' +
+            '<dc:creator>Haruki Murakami</dc:creator>' +
+            '<dc:publisher>Klim</dc:publisher>' +
+            '<dc:date>2001</dc:date>' +
+            '<dc:identifier>870970,23645564</dc:identifier>' +
+            '<dc:identifier>ISBN:87-7724-857-0</dc:identifier>' +
+            '<dc:source>Nejimaki-dori kuronikure</dc:source>' +
+            '<dc:language>Dansk</dc:language>' +
+        '</oai_dc:dc>'
+    );
+
+    Assert.equalValue( "createDcXml", MarcXchangeToOaiDc.createDcXml( recordString ), expected );
+
+} );
+
+UnitTest.addFixture( "test addDcTitleElement", function( ) {
+
+    var record = new Record();
+    record.fromString(
+        '001 00 *a2 364 556 4 *b870970 *c20160525133413 *d20010824 *fa\n' +
+        '245 00 *aTraekopfuglens kroenike *eHaruki Murakami *foversat af Mette Holm\n'
+    );
+
+    var xml = XmlUtil.fromString(
+        '<oai_dc:dc ' +
+        'xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" ' +
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd"/>'
+    );
+
+    var actual = MarcXchangeToOaiDc.__callElementMethod( MarcXchangeToOaiDc.addDcTitleElement, xml, record );
+
+    var expected = XmlUtil.fromString(
+        '<oai_dc:dc ' +
+        'xmlns:dc="http://purl.org/dc/elements/1.1/" ' +
+        'xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" ' +
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">' +
+        '<dc:title>Traekopfuglens kroenike</dc:title>' +
+        '</oai_dc:dc>'
+    );
+
+    Assert.equalValue( "addDcTitleElement", actual, expected );
+
+} );
+
+
+
+
---
+++ formatter/src/main/resources/javascript/MarcXchangeToOaiDc.use.js
@@ -0,0 +1,113 @@
+/** @file Module that creates simple Dublin Core elements based on marcxchange record. */
+
+use( "Log" );
+use( "Marc" );
+use( "MarcXchange" );
+use( "XmlNamespaces" );
+use( "XmlUtil" );
+
+EXPORTED_SYMBOLS = [ 'MarcXchangeToOaiDc' ];
+
+/**
+ * Module with functions that creates simple Dublin Core elements for OAI harvest of records from RawRepo.
+ *
+ * This module contains functions to create dc record
+ *
+ * @type {namespace}
+ * @namespace
+ */
+
+var MarcXchangeToOaiDc = function() {
+
+    Log.info( "Entering MarcXchangeToOaiDc module" );
+
+
+    /**
+     * Function that is entry to create a complete Dublin Core Record.
+     *
+     * @syntax MarcXchangeToOaiDc.createDcXml( marcXrecord )
+     * @param {String} marcXrecord the marcxchange record to create DC from
+     * @return {Document} the created DC record
+     * @type {function}
+     * @function
+     * @name MarcXchangeToOaiDc.createDcXml
+     */
+    function createDcXml( marcXrecord ) {
+
+        Log.trace( "Entering MarcXchangeToOaiDc.createDcXml" );
+
+        var marcRecord = MarcXchange.marcXchangeToMarcRecord( marcXrecord );
+        var oaiDcXml = XmlUtil.createDocument( "dc", XmlNamespaces.oai_dc );
+
+        var map = new MatchMap( );
+        oaiDcXml = MarcXchangeToOaiDc.addDcTitleElement( oaiDcXml, map );
+        //call more functions
+        marcRecord.eachFieldMap( map );
+
+        Log.trace( "Leaving MarcXchangeToOaiDc.createDcXml" );
+
+        return oaiDcXml;
+
+    }
+
+    /**
+     * Function that puts eachField function on map to create dc:title from field 245 subfield a.
+     *
+     * @syntax MarcXchangeToOaiDc.addDcTitleElement( oaiDcXml, map )
+     * @param {Document} oaiDcXml
+     * @param {MatchMap} map
+     * @type {function}
+     * @function
+     * @name MarcXchangeToOaiDc.addDcTitleElement
+     */
+    function addDcTitleElement( oaiDcXml, map ) {
+
+        Log.trace( "Entering MarcXchangeToOaiDc.createDcTitle" );
+
+        map.put( "245", function( field ) {
+            var titles = [];
+            field.eachSubfield( "a", function( field, subfield ) {
+                titles.push( subfield.value );
+            } );
+            var dcTitleValue = titles.join( ". " );
+            XmlUtil.appendChildElement( oaiDcXml, "title", XmlNamespaces.dc, dcTitleValue );
+        } );
+
+        Log.trace( "Leaving MarcXchangeToOaiDc.createDcTitle" );
+
+    }
+
+
+    /**
+     * Create a MatchMap, add mapping functions from a specific element method and build the dcxml from a record.
+     * To be used when unit testing individual functions that adds elements to xml.
+     *
+     *
+     * @type {function}
+     * @syntax MarcXchangeToOaiDc.__callElementMethod( func, xml, record )
+     * @param {Function} func the add-element function to call
+     * @param {Document} xml the xml to add element to
+     * @param {Record} record The record from which to create the element
+     * @return {Document} xml with added element
+     * @name MarcXchangeToOaiDc.__callElementMethod
+     * @function
+     */
+    function __callElementMethod( func, xml, record ) {
+
+        var map = new MatchMap();
+        func( xml, map );
+        record.eachFieldMap( map );
+
+        return xml;
+    }
+
+
+    Log.info( "Leaving MarcXchangeToOaiDc module" );
+
+    return {
+        createDcXml: createDcXml,
+        addDcTitleElement: addDcTitleElement,
+        __callElementMethod: __callElementMethod
+    }
+
+}();
