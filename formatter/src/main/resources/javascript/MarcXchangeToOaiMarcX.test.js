1455b08821d9dc0d74dbde2c1c7fe8dd6c00ba3f
lskdbc <lsk@dbc.dk>
2017-08-24 20:33:53 +0200

    rawrepo-oai reestablishing module headlines with copyright information

--- formatter/src/main/resources/javascript/MarcXchangeToOaiMarcX.test.js
+++ formatter/src/main/resources/javascript/MarcXchangeToOaiMarcX.test.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ * See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
 /** @file Module that contains unit tests for functions in MarcXchangeToOaiMarcX module */
 
 use( "MarcXchangeToOaiMarcX" );
